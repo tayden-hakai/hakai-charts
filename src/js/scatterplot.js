64d6ea32840eac8640a04d787b17a43a9ca94f34
tayden-hakai <tayden.denouden@hakai.org>
2016-01-08 12:57:36 -0800

    Change scatterplot code to use ES6 shorthand and best practices

--- src/js/scatterplot.js
+++ src/js/scatterplot.js
@@ -1,4 +1,3 @@
-'use strict';
 /* global d3 ss */
 /* eslint-env browser */
 
@@ -9,30 +8,87 @@
  * @param {string} parent | {DOM element} parent - A dom element to append the vis to
  * @return {object} scatterplot
  */
-module.exports = function(parent) {
-  var _width;
-  var _height;
-  var _margin;
-  var _data;
-  var _x = d3.scale.linear();
-  var _y = d3.scale.linear();
-  var _xLog = false;
-  var _yLog = false;
-  var _xAxis;
-  var _yAxis;
-  var _xAccessor;
-  var _yAccessor;
-  var _xLabel;
-  var _yLabel;
-  var _color = d3.scale.category10();
-  var _colorAccessor = function() { return 0; };
-  var _keyAccessor = function(d) { return d.key; };
-  var _radius = 5;
-  var _regLine;
-  var _rSquared = 1;
-  var _correlation = 1;
-  var _covariance = 1;
-  var _svg;
+module.exports = function scatterplot(parent) {
+  let _width;
+  let _height;
+  let _margin;
+  let _data;
+  let _x = d3.scale.linear();
+  let _y = d3.scale.linear();
+  let _xLog = false;
+  let _yLog = false;
+  let _xAxis;
+  let _yAxis;
+  let _xAccessor;
+  let _yAccessor;
+  let _xLabel;
+  let _yLabel;
+  let _color = d3.scale.category10();
+  let _colorAccessor = () => 0;
+  let _keyAccessor = d => d.key;
+  let _radius = 5;
+  let _regLine;
+  let _rSquared = 1;
+  let _correlation = 1;
+  let _covariance = 1;
+  let _svg;
+
+  /**
+   * Return the base ten log of a Number
+   * @param {number} d - A number to apply the operation on
+   * @return {number} log base 10 of d
+   */
+  function log10(d) {
+    return Math.log(d) / Math.log(10);
+  }
+
+  /**
+   * Given an x value of un-transformed data (no log transform etc.),
+   * get the untransformed y coordinate from the regression line
+   * @param {number} x - The untransformed x value in linear space
+   * @param {function} regLine - The regression line calculated using
+   *    a transformed dataset that accounted for the log scales
+   * @return {number} y - The untransformed y value in linear space to plot on the chart
+   */
+  function getY(x, regLine) {
+    const y = regLine(_xLog ? log10(x) : x);
+    return _yLog ? Math.pow(10, y) : y;
+  }
+
+  /**
+   * Calculate all chart statistical values and regresslion linearRegression
+   * @param {Object[]} data - The dataset used to calculate the statistics
+   * @return {Object} stats - An object with all calculated statistics
+   */
+  function calculateStats(data) {
+    // Get regression line formula
+    const ssData = data.map(function ssData(d) {
+      const xD = _xLog ? log10(_xAccessor(d)) : _xAccessor(d);
+      const yD = _yLog ? log10(_yAccessor(d)) : _yAccessor(d);
+      return [xD, yD];
+    });
+    const mb = ss.linearRegression(ssData);
+    const regLine = ss.linearRegressionLine(mb);
+
+    // Calculated statistics
+    const rSquared = ss.rSquared(ssData, regLine);
+    const correlation = ss.sampleCorrelation(
+      ssData.map(d => d[0]),
+      ssData.map(d => d[1])
+    );
+    const covariance = ss.sampleCovariance(
+      ssData.map(d => d[0]),
+      ssData.map(d => d[1])
+    );
+
+    return {
+      reg: mb,
+      regLine,
+      rSquared,
+      correlation,
+      covariance,
+    };
+  }
 
   function _chart() {
     // Create svg object
@@ -43,11 +99,9 @@ module.exports = function(parent) {
       .attr('transform', 'translate(' + _margin.left + ',' + _margin.top + ')');
 
     // Create cleaned dataset that doesn't include non numeric or log(0) values
-    var cleanData = _data.filter(function(d) {
-      return !(isNaN(_xAccessor(d)) || isNaN(_yAccessor(d)));
-    }).filter(function(d) {
-      return !((_xLog && _xAccessor(d) === 0) || (_yLog && _yAccessor(d) === 0));
-    });
+    const cleanData = _data
+        .filter(d => !(isNaN(_xAccessor(d)) || isNaN(_yAccessor(d))))
+        .filter(d => !((_xLog && _xAccessor(d) === 0) || (_yLog && _yAccessor(d) === 0)));
 
     // Set x and y axis based on selected attributes
     _x.domain(d3.extent(cleanData, _xAccessor))
@@ -104,7 +158,7 @@ module.exports = function(parent) {
         .attr('height', _height);
 
     // Calculate statistics and regression line
-    var stats = calculateStats(cleanData);
+    const stats = calculateStats(cleanData);
     _regLine = stats.regLine;
     _rSquared = stats.rSquared;
     _correlation = stats.correlation;
@@ -124,37 +178,35 @@ module.exports = function(parent) {
         .attr('y2', _y(getY(_x.domain()[1], _regLine)));
 
     // Add data marks to chart
-    var marks = _svg.selectAll('g.mark')
+    const marks = _svg.selectAll('g.mark')
         .data(cleanData)
       .enter().append('g')
         .attr('class', 'mark');
 
     marks.append('circle')
-        .attr('cx', function(d) { return _x(_xAccessor(d)); })
-        .attr('cy', function(d) { return _y(_yAccessor(d)); })
+        .attr('cx', d => _x(_xAccessor(d)))
+        .attr('cy', d => _y(_yAccessor(d)))
         .attr('r', _radius)
-        .attr('fill', function(d) { return _color(_colorAccessor(d)); });
+        .attr('fill', d => _color(_colorAccessor(d)));
 
     marks.append('text')
         .text(_keyAccessor)
-        .attr('x', function(d) { return _x(_xAccessor(d)); })
-        .attr('y', function(d) { return _y(_yAccessor(d)); })
+        .attr('x', d => _x(_xAccessor(d)))
+        .attr('y', d => _y(_yAccessor(d)))
         .attr('text-anchor', 'end')
         .attr('dy', -5)
         .attr('dx', -2);
   }
 
-  _chart.render = function() {
+  _chart.render = function render() {
     this.call();
     return _chart;
   };
-  _chart.redraw = function() {
+  _chart.redraw = function redraw() {
     // Create cleaned dataset that doesn't include non numeric or log(0) values
-    var cleanData = _data.filter(function(d) {
-      return !(isNaN(_xAccessor(d)) || isNaN(_yAccessor(d)));
-    }).filter(function(d) {
-      return !((_xLog && _xAccessor(d) === 0) || (_yLog && _yAccessor(d) === 0));
-    });
+    const cleanData = _data
+        .filter(d => !(isNaN(_xAccessor(d)) || isNaN(_yAccessor(d))))
+        .filter(d => !((_xLog && _xAccessor(d) === 0) || (_yLog && _yAccessor(d) === 0)));
 
     // Update x and y domain
     _x.domain(d3.extent(cleanData, _xAccessor))
@@ -167,7 +219,7 @@ module.exports = function(parent) {
     _yAxis.scale(_y);
 
     // Define consistent transition duration
-    var t = 1500;
+    const t = 1500;
 
     // Update axes
     _svg.select('.x.axis')
@@ -184,7 +236,7 @@ module.exports = function(parent) {
         .text(_yLabel);
 
     // Calculate statistics and regression line
-    var stats = calculateStats(cleanData);
+    const stats = calculateStats(cleanData);
     _regLine = stats.regLine;
     _rSquared = stats.rSquared;
     _correlation = stats.correlation;
@@ -199,36 +251,36 @@ module.exports = function(parent) {
         .attr('y2', _y(getY(_x.domain()[1], _regLine)));
 
     // Update data and mark positions
-    var marks = _svg.selectAll('g.mark')
+    const marks = _svg.selectAll('g.mark')
         .data(cleanData, _keyAccessor);
 
     // Update
     marks.selectAll('circle')
       .transition().duration(t)
-        .attr('cx', function(d) { return _x(_xAccessor(d)); })
-        .attr('cy', function(d) { return _y(_yAccessor(d)); })
+        .attr('cx', d => _x(_xAccessor(d)))
+        .attr('cy', d => _y(_yAccessor(d)))
         .attr('r', _radius)
-        .attr('fill', function(d) { return _color(_colorAccessor(d)); });
+        .attr('fill', d => _color(_colorAccessor(d)));
 
     marks.selectAll('text')
       .transition().duration(t)
-        .attr('x', function(d) { return _x(_xAccessor(d)); })
-        .attr('y', function(d) { return _y(_yAccessor(d)); });
+        .attr('x', d => _x(_xAccessor(d)))
+        .attr('y', d => _y(_yAccessor(d)));
 
     // Enter
-    var g = marks.enter().append('g')
+    const g = marks.enter().append('g')
         .attr('class', 'mark');
 
     g.append('circle')
-        .attr('cx', function(d) { return _x(_xAccessor(d)); })
-        .attr('cy', function(d) { return _y(_yAccessor(d)); })
+        .attr('cx', d => _x(_xAccessor(d)))
+        .attr('cy', d => _y(_yAccessor(d)))
         .attr('r', _radius)
-        .attr('fill', function(d) { return _color(_colorAccessor(d)); });
+        .attr('fill', d => _color(_colorAccessor(d)));
 
     g.append('text')
         .text(_keyAccessor)
-        .attr('x', function(d) { return _x(_xAccessor(d)); })
-        .attr('y', function(d) { return _y(_yAccessor(d)); })
+        .attr('x', d => _x(_xAccessor(d)))
+        .attr('y', d => _y(_yAccessor(d)))
         .attr('text-anchor', 'end')
         .attr('dy', -5)
         .attr('dx', -2);
@@ -238,144 +290,87 @@ module.exports = function(parent) {
 
     return _chart;
   };
-  _chart.width = function(val) {
+  _chart.width = function width(val) {
     if (!arguments.length) { return _width; }
     _width = val;
     return _chart;
   };
-  _chart.height = function(val) {
+  _chart.height = function height(val) {
     if (!arguments.length) { return _height; }
     _height = val;
     return _chart;
   };
-  _chart.margin = function(val) {
+  _chart.margin = function margin(val) {
     if (!arguments.length) { return _margin; }
     _margin = val;
     return _chart;
   };
-  _chart.data = function(val) {
+  _chart.data = function data(val) {
     if (!arguments.length) { return _data; }
     _data = val;
     return _chart;
   };
-  _chart.xAccessor = function(val) {
+  _chart.xAccessor = function xAccessor(val) {
     if (!arguments.length) { return _xAccessor; }
     _xAccessor = val;
     return _chart;
   };
-  _chart.yAccessor = function(val) {
+  _chart.yAccessor = function yAccessor(val) {
     if (!arguments.length) { return _yAccessor; }
     _yAccessor = val;
     return _chart;
   };
-  _chart.xLabel = function(val) {
+  _chart.xLabel = function xLabel(val) {
     if (!arguments.length) { return _xLabel; }
     _xLabel = val;
     return _chart;
   };
-  _chart.yLabel = function(val) {
+  _chart.yLabel = function yLabel(val) {
     if (!arguments.length) { return _yLabel; }
     _yLabel = val;
     return _chart;
   };
-  _chart.xLog = function(val) {
+  _chart.xLog = function xLog(val) {
     if (!arguments.length) { return _xLog; }
     _xLog = val;
     _x = _xLog ? d3.scale.log() : d3.scale.linear();
     return _chart;
   };
-  _chart.yLog = function(val) {
+  _chart.yLog = function yLog(val) {
     if (!arguments.length) { return _yLog; }
     _yLog = val;
     _y = _yLog ? d3.scale.log() : d3.scale.linear();
     return _chart;
   };
-  _chart.color = function(val) {
+  _chart.color = function color(val) {
     if (!arguments.length) { return _color; }
     _color = val;
     return _chart;
   };
-  _chart.colorAccessor = function(val) {
+  _chart.colorAccessor = function colorAccessor(val) {
     if (!arguments.length) { return _colorAccessor; }
     _colorAccessor = val;
     return _chart;
   };
-  _chart.keyAccessor = function(val) {
+  _chart.keyAccessor = function keyAccessor(val) {
     if (!arguments.length) { return _keyAccessor; }
     _keyAccessor = val;
     return _chart;
   };
-  _chart.radius = function(val) {
+  _chart.radius = function radius(val) {
     if (!arguments.length) { return _radius; }
     _radius = val;
     return _chart;
   };
-  _chart.rSquared = function() {
+  _chart.rSquared = function rSquared() {
     return _rSquared;
   };
-  _chart.correlation = function() {
+  _chart.correlation = function correlation() {
     return _correlation;
   };
-  _chart.covariance = function() {
+  _chart.covariance = function covariance() {
     return _covariance;
   };
 
   return _chart;
-
-  /**
-   * Return the base ten log of a Number
-   * @param {number} d - A number to apply the operation on
-   * @return {number} log base 10 of d
-   */
-  function log10(d) {
-    return Math.log(d) / Math.log(10);
-  }
-
-  /**
-   * Given an x value of un-transformed data (no log transform etc.),
-   * get the untransformed y coordinate from the regression line
-   * @param {number} x - The untransformed x value in linear space
-   * @param {function} regLine - The regression line calculated using
-   *    a transformed dataset that accounted for the log scales
-   * @return {number} y - The untransformed y value in linear space to plot on the chart
-   */
-  function getY(x, regLine) {
-    var y = regLine(_xLog ? log10(x) : x);
-    return _yLog ? Math.pow(10, y) : y;
-  }
-
-  /**
-   * Calculate all chart statistical values and regresslion linearRegression
-   * @param {Object[]} data - The dataset used to calculate the statistics
-   * @return {Object} stats - An object with all calculated statistics
-   */
-  function calculateStats(data) {
-    // Get regression line formula
-    var ssData = data.map(function(d) {
-      var xD = _xLog ? log10(_xAccessor(d)) : _xAccessor(d);
-      var yD = _yLog ? log10(_yAccessor(d)) : _yAccessor(d);
-      return [xD, yD];
-    });
-    var mb = ss.linearRegression(ssData);
-    var regLine = ss.linearRegressionLine(mb);
-
-    // Calculated statistics
-    var rSquared = ss.rSquared(ssData, regLine);
-    var correlation = ss.sampleCorrelation(
-      ssData.map(function(d) {return d[0]; }),
-      ssData.map(function(d) {return d[1]; })
-    );
-    var covariance = ss.sampleCovariance(
-      ssData.map(function(d) {return d[0]; }),
-      ssData.map(function(d) {return d[1]; })
-    );
-
-    return {
-      reg: mb,
-      regLine: regLine,
-      rSquared: rSquared,
-      correlation: correlation,
-      covariance: covariance
-    };
-  }
 };
