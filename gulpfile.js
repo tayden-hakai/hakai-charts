2bf8745da60167d104b0da95617a1841df027a68
tayden-hakai <tayden.denouden@hakai.org>
2016-01-12 17:35:29 -0800

    Update webpack config

--- gulpfile.js
+++ gulpfile.js
@@ -17,16 +17,24 @@ gulp.task('document', function document() {
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('webpack', function buildProd() {
-  webpack(webpackConfig);
+gulp.task('webpack', function buildProd(callback) {
+  webpack(webpackConfig, function(err, stats) {
+    if (err) throw new gutil.PluginError('webpack', err);
+    gutil.log('[webpack]', stats.toString());
+    callback();
+  });
 });
 
 gulp.task('webpack-dev-server', function webpackServer() {
   const conf = webpackConfig;
   conf.devtool = 'source-map';
-  // Start a webpack-dev-server
-  const compiler = webpack(conf);
 
+  const compiler = webpack(conf, function(err, stats) {
+    if (err) throw new gutil.PluginError('webpack', err);
+    gutil.log('[webpack-dev-server]', stats.toString());
+  });
+
+  // Start a webpack-dev-server
   new WebpackDevServer(
     compiler,
     { contentBase: './', hot: true }
