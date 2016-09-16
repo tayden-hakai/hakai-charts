25f0b43b3fa95a2d6eb6f7a93877daf84efe9f53
Taylor Denouden <taylor.denouden@hakai.org>
2016-09-16 13:11:28 -0700

    Fix lint errors in tests

--- src/js/test/scatterplot.spec.js
+++ src/js/test/scatterplot.spec.js
@@ -4,16 +4,16 @@ import { schemeCategory10, schemeCategory20 } from 'd3-scale';
 import { expect } from 'chai';
 import scatterplot from '../scatterplot.js';
 
-describe('Scatterplot', function () {
+describe('Scatterplot', () => {
   let chart;
 
-  beforeEach(function () {
+  beforeEach(() => {
     chart = scatterplot();
   });
 
 
-  describe('height', function () {
-    it('should set and get the chart height correctly', function () {
+  describe('height', () => {
+    it('should set and get the chart height correctly', () => {
       chart.height(500);
       expect(chart.height()).to.equal(500);
       chart.height(231);
@@ -21,8 +21,8 @@ describe('Scatterplot', function () {
     });
   });
 
-  describe('width', function () {
-    it('should set and get the chart width correctly', function () {
+  describe('width', () => {
+    it('should set and get the chart width correctly', () => {
       chart.width(500);
       expect(chart.width()).to.equal(500);
       chart.width(684);
@@ -30,8 +30,8 @@ describe('Scatterplot', function () {
     });
   });
 
-  describe('margin', function () {
-    it('should set and get the chart margin correctly', function () {
+  describe('margin', () => {
+    it('should set and get the chart margin correctly', () => {
       chart.margin({ top: 1, right: 2, bottom: 3, left: 4 });
       expect(chart.margin()).to.deep.equal({ top: 1, right: 2, bottom: 3, left: 4 });
       chart.margin({ top: 12, right: 73, bottom: 51, left: 23 });
@@ -39,8 +39,8 @@ describe('Scatterplot', function () {
     });
   });
 
-  describe('data', function () {
-    it('should set and get the chart data correctly', function () {
+  describe('data', () => {
+    it('should set and get the chart data correctly', () => {
       chart.data([{ x: 1, y: 2 }]);
       expect(chart.data()).to.deep.equal([{ x: 1, y: 2 }]);
       chart.data([{ x: 1, y: 2 }, { x: 2, y: 4 }, { x: 74, y: 10000 }]);
@@ -48,51 +48,51 @@ describe('Scatterplot', function () {
     });
   });
 
-  describe('xAccessor', function () {
+  describe('xAccessor', () => {
     const data = { x: 1, value: 2 };
-    it('should have a sensible default', function () {
+    it('should have a sensible default', () => {
       const xAccessor = chart.xAccessor();
       expect(xAccessor(data)).to.equal(1);
     });
-    it('should set and get an xAccessor correctly', function () {
-      chart.xAccessor(function (d) { return d.value; });
+    it('should set and get an xAccessor correctly', () => {
+      chart.xAccessor(d => d.value);
       const xAccessor = chart.xAccessor();
       expect(xAccessor(data)).to.equal(2);
     });
   });
 
-  describe('yAccessor', function () {
+  describe('yAccessor', () => {
     const data = { y: 1, value: 2 };
-    it('should have a sensible default', function () {
+    it('should have a sensible default', () => {
       const yAccessor = chart.yAccessor();
       expect(yAccessor(data)).to.equal(1);
     });
-    it('should set and get an yAccessor correctly', function () {
-      chart.yAccessor(function (d) { return d.value; });
+    it('should set and get an yAccessor correctly', () => {
+      chart.yAccessor(d => d.value);
       const yAccessor = chart.yAccessor();
       expect(yAccessor(data)).to.equal(2);
     });
   });
 
-  describe('xLabel', function () {
-    it('should set and get an xLabel correctly', function () {
+  describe('xLabel', () => {
+    it('should set and get an xLabel correctly', () => {
       chart.xLabel('Some x string');
       expect(chart.xLabel()).to.equal('Some x string');
     });
   });
 
-  describe('yLabel', function () {
-    it('should set and get an yLabel correctly', function () {
+  describe('yLabel', () => {
+    it('should set and get an yLabel correctly', () => {
       chart.yLabel('Some x string');
       expect(chart.yLabel()).to.equal('Some x string');
     });
   });
 
-  describe('xLog', function () {
-    it('should default to false', function () {
+  describe('xLog', () => {
+    it('should default to false', () => {
       expect(chart.xLog()).to.be.false;
     });
-    it('should set and get a value correctly', function () {
+    it('should set and get a value correctly', () => {
       chart.xLog(true);
       expect(chart.xLog()).to.be.true;
       chart.xLog(false);
@@ -100,11 +100,11 @@ describe('Scatterplot', function () {
     });
   });
 
-  describe('yLog', function () {
-    it('should default to false', function () {
+  describe('yLog', () => {
+    it('should default to false', () => {
       expect(chart.yLog()).to.be.false;
     });
-    it('should set and get a value correctly', function () {
+    it('should set and get a value correctly', () => {
       chart.yLog(true);
       expect(chart.yLog()).to.be.true;
       chart.yLog(false);
@@ -112,61 +112,61 @@ describe('Scatterplot', function () {
     });
   });
 
-  describe('color', function () {
-    it('should be have a sensible default', function () {
+  describe('color', () => {
+    it('should be have a sensible default', () => {
       expect(chart.color()).to.deep.equal(schemeCategory10);
     });
-    it('should set and get a color scale correctly', function () {
+    it('should set and get a color scale correctly', () => {
       chart.color(schemeCategory20);
       expect(chart.color()).to.deep.equal(schemeCategory20);
     });
   });
 
-  describe('colorAccessor', function () {
-    it('should default to 0', function () {
+  describe('colorAccessor', () => {
+    it('should default to 0', () => {
       expect(chart.colorAccessor()(10)).to.equal(0);
       expect(chart.colorAccessor()(1231)).to.equal(0);
       expect(chart.colorAccessor()()).to.equal(0);
     });
-    it('should set and get a colorAccessor correctly', function () {
-      chart.colorAccessor(function (d) { return d.color; });
+    it('should set and get a colorAccessor correctly', () => {
+      chart.colorAccessor(d => d.color);
       const data = { x: 1, y: 2, color: 54 };
       expect(chart.colorAccessor()(data)).to.equal(54);
     });
   });
 
-  describe('keyAccessor', function () {
+  describe('keyAccessor', () => {
     const data = { x: 1, value: 2, key: 0 };
-    it('should have a sensible default', function () {
+    it('should have a sensible default', () => {
       const keyAccessor = chart.keyAccessor();
       expect(keyAccessor(data)).to.equal(0);
     });
-    it('should set and get an keyAccessor correctly', function () {
-      chart.keyAccessor(function (d) { return d.value; });
+    it('should set and get an keyAccessor correctly', () => {
+      chart.keyAccessor(d => d.value);
       const keyAccessor = chart.keyAccessor();
       expect(keyAccessor(data)).to.equal(2);
     });
   });
 
-  describe('radius', function () {
-    it('should default to 5', function () {
+  describe('radius', () => {
+    it('should default to 5', () => {
       expect(chart.radius()).to.equal(5);
     });
-    it('should set and get an radius correctly', function () {
+    it('should set and get an radius correctly', () => {
       chart.radius(20);
       expect(chart.radius()).to.equal(20);
     });
   });
 
-  xdescribe('rSquared', function () {
+  xdescribe('rSquared', () => {
     it('should return the correct rSquared value');
   });
 
-  xdescribe('correlation', function () {
+  xdescribe('correlation', () => {
     it('should return the correct correlation value');
   });
 
-  xdescribe('covariance', function () {
+  xdescribe('covariance', () => {
     it('should return the correct covariance value');
   });
 });
--- src/js/test/vertical_line.spec.js
+++ src/js/test/vertical_line.spec.js
@@ -3,16 +3,16 @@
 import { expect } from 'chai';
 import verticalLine from '../vertical_line.js';
 
-describe('Vertical Line', function () {
+describe('Vertical Line', () => {
   let chart;
 
-  beforeEach(function () {
+  beforeEach(() => {
     chart = verticalLine();
   });
 
 
-  describe('height', function () {
-    it('should set and get the chart height correctly', function () {
+  describe('height', () => {
+    it('should set and get the chart height correctly', () => {
       chart.height(500);
       expect(chart.height()).to.equal(500);
       chart.height(231);
@@ -20,8 +20,8 @@ describe('Vertical Line', function () {
     });
   });
 
-  describe('width', function () {
-    it('should set and get the chart width correctly', function () {
+  describe('width', () => {
+    it('should set and get the chart width correctly', () => {
       chart.width(500);
       expect(chart.width()).to.equal(500);
       chart.width(684);
@@ -29,8 +29,8 @@ describe('Vertical Line', function () {
     });
   });
 
-  describe('margin', function () {
-    it('should set and get the chart margin correctly', function () {
+  describe('margin', () => {
+    it('should set and get the chart margin correctly', () => {
       chart.margin({ top: 1, right: 2, bottom: 3, left: 4 });
       expect(chart.margin()).to.deep.equal({ top: 1, right: 2, bottom: 3, left: 4 });
       chart.margin({ top: 12, right: 73, bottom: 51, left: 23 });
@@ -38,8 +38,8 @@ describe('Vertical Line', function () {
     });
   });
 
-  describe('margin2', function () {
-    it('should set and get the chart margin correctly', function () {
+  describe('margin2', () => {
+    it('should set and get the chart margin correctly', () => {
       chart.margin2({ top: 1, right: 2, bottom: 3, left: 4 });
       expect(chart.margin2()).to.deep.equal({ top: 1, right: 2, bottom: 3, left: 4 });
       chart.margin2({ top: 12, right: 73, bottom: 51, left: 23 });
@@ -47,8 +47,8 @@ describe('Vertical Line', function () {
     });
   });
 
-  describe('data', function () {
-    it('should set and get the chart data correctly', function () {
+  describe('data', () => {
+    it('should set and get the chart data correctly', () => {
       chart.data([{ x: 1, y: 2 }]);
       expect(chart.data()).to.deep.equal([{ x: 1, y: 2 }]);
       chart.data([{ x: 1, y: 2 }, { x: 2, y: 4 }, { x: 74, y: 10000 }]);
@@ -56,72 +56,72 @@ describe('Vertical Line', function () {
     });
   });
 
-  describe('xAccessor', function () {
+  describe('xAccessor', () => {
     const data = { x: 1, value: 2 };
-    it('should have a sensible default', function () {
+    it('should have a sensible default', () => {
       const xAccessor = chart.xAccessor();
       expect(xAccessor(data)).to.equal(1);
     });
-    it('should set and get an xAccessor correctly', function () {
-      chart.xAccessor(function (d) { return d.value; });
+    it('should set and get an xAccessor correctly', () => {
+      chart.xAccessor(d => d.value);
       const xAccessor = chart.xAccessor();
       expect(xAccessor(data)).to.equal(2);
     });
   });
 
-  describe('yAccessor', function () {
+  describe('yAccessor', () => {
     const data = { y: 1, value: 2 };
-    it('should have a sensible default', function () {
+    it('should have a sensible default', () => {
       const yAccessor = chart.yAccessor();
       expect(yAccessor(data)).to.equal(1);
     });
-    it('should set and get an yAccessor correctly', function () {
-      chart.yAccessor(function (d) { return d.value; });
+    it('should set and get an yAccessor correctly', () => {
+      chart.yAccessor(d => d.value);
       const yAccessor = chart.yAccessor();
       expect(yAccessor(data)).to.equal(2);
     });
   });
 
-  describe('xLabel', function () {
-    it('should set and get an xLabel correctly', function () {
+  describe('xLabel', () => {
+    it('should set and get an xLabel correctly', () => {
       chart.xLabel('Some x string');
       expect(chart.xLabel()).to.equal('Some x string');
     });
   });
 
-  describe('yLabel', function () {
-    it('should set and get an yLabel correctly', function () {
+  describe('yLabel', () => {
+    it('should set and get an yLabel correctly', () => {
       chart.yLabel('Some x string');
       expect(chart.yLabel()).to.equal('Some x string');
     });
   });
 
-  xdescribe('color', function () {
+  xdescribe('color', () => {
     it('should be have a sensible default');
     it('should set and get a color correctly');
   });
 
-  describe('keyAccessor', function () {
+  describe('keyAccessor', () => {
     const data = { x: 1, value: 2, key: 0 };
-    it('should have a sensible default', function () {
+    it('should have a sensible default', () => {
       const keyAccessor = chart.keyAccessor();
       expect(keyAccessor(data)).to.equal(0);
     });
-    it('should set and get an keyAccessor correctly', function () {
-      chart.keyAccessor(function (d) { return d.x; });
+    it('should set and get an keyAccessor correctly', () => {
+      chart.keyAccessor(d => d.x);
       const keyAccessor = chart.keyAccessor();
       expect(keyAccessor(data)).to.equal(1);
     });
   });
 
-  describe('valueAccessor', function () {
+  describe('valueAccessor', () => {
     const data = { x: 1, value: 2, key: 0 };
-    it('should have a sensible default', function () {
+    it('should have a sensible default', () => {
       const valueAccessor = chart.valueAccessor();
       expect(valueAccessor(data)).to.equal(2);
     });
-    it('should set and get an valueAccessor correctly', function () {
-      chart.valueAccessor(function (d) { return d.x; });
+    it('should set and get an valueAccessor correctly', () => {
+      chart.valueAccessor(d => d.x);
       const valueAccessor = chart.valueAccessor();
       expect(valueAccessor(data)).to.equal(1);
     });
